79059a4c25d792394b78bde9474d8cf73d2f6fdb
Heder L Santos <73147296+MorcegoH@users.noreply.github.com>
2021-11-03 20:54:58 -0300

    Atualização
    Aula 03-11

--- Loja SQL/loja.sql
+++ Loja SQL/loja.sql
@@ -1,5 +1,5 @@
 /**
-	Atividade Carrinho
+	Atividade E-commerce
     @author Aluno Heder Santos
 */
 
@@ -7,112 +7,127 @@
 
 create database loja;
 use loja;
-create table carrinho 
-(
-	sku int primary key auto_increment,
-    produto varchar(150) not null,
-    quantidade int not null,
-    valor int not null
-);
 
-alter table carrinho change valor valor decimal(10,2) not null;
-
-describe carrinho;
-
--- CRUD (Create)
-insert into carrinho (produto,quantidade,valor) values ('caneta bic CX30',10,17.50);
-insert into carrinho (produto,quantidade,valor) values ('Cheetos Requeijao',2,9.20);
-insert into carrinho (produto,quantidade,valor) values ('Alexa',1,20.7);
-insert into carrinho (produto,quantidade,valor) values ('Soju',5,35.9);
-insert into carrinho (produto,quantidade,valor) values ('Yakissoba',6,45.9);
-insert into carrinho (produto,quantidade,valor) values ('Celular',2,1599.0);
-insert into carrinho (produto,quantidade,valor) values ('Alforge',1,350.9);
-
--- CRUD (Read)
-select * from carrinho;
-
--- Operações Matematicas no banco de dados
-select (valor * quantidade) as parcial from carrinho;
-select sum(valor * quantidade) as total from carrinho;
-
--- Selects CRUD
-select sku, produto from carrinho;
-select quantidade, produto, valor from carrinho;
-select sku, valor, produto from carrinho;
-
--- Select Colunas
-select * from carrinho order by produto;
-select * from carrinho order by quantidade;
-select * from carrinho order by sku;
-
--- descrescente;
-select sku, produto from carrinho order by sku desc;
-select valor, sku from carrinho order by valor desc;
-select sku, produto, valor from carrinho order by produto desc;
-
--- Filtros
-select * from carrinho where sku= 1;
-select * from carrinho where valor= 350.9;
-select * from carrinho where produto='Cheetos Requeijao';
-
--- Update - ATENÇÃO!! Não esquece do comando WHERE. Usar sempre o ID para evitar problemas.
-update carrinho set quantidade=7 where sku=3;
-update carrinho set quantidade=63 where sku=6;
-update carrinho set quantidade=10 where sku=4;
-update carrinho set quantidade=2 where sku=6;
-
--- Delete - ATENÇÃO!! Não esquece do comando WHERE. Usar sempre o ID para evitar problemas.
-delete from carrinho where sku=6;
-delete from carrinho where sku=2;
-
--- CRIAÇÃO TABELA ESTOQUE
-create table estoque (
+create table clientes(
+	idcli int primary key auto_increment,
+    nome varchar(50) not null,
+    email varchar(50) unique not null,
+    senha varchar(250) not null
+    );
+    
+    alter table clientes drop column cidade;
+    
+    insert into clientes (nome,email,senha) values
+    ('Mikhael Santos','mikah.sanchez@git.io',md5('123456'));
+     insert into clientes (nome,email,senha) values
+    ('Ana Rodrigues','ana.silva@git.io',md5('123456'));
+     insert into clientes (nome,email,senha) values
+    ('Ayrton Santos','senna.f1@git.io',md5('123456'));
+     insert into clientes (nome,email,senha) values
+    ('Asap Sic','asa.sz@git.io',md5('123456'));
+     insert into clientes (nome,email,senha) values
+    ('Apolo Socks','appl.sza@git.io',md5('123456'));
+     insert into clientes (nome,email,senha) values
+    ('Augustus Silenio','august.cesar@git.io',md5('123456'));
+    
+    
+    -- Login
+    -- and (o select só será executado se as duas condições existirem.
+    select * from clientes where email='mikah.sanchez@git.io' and senha=md5('123456');
+    -- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    
+    
+	describe clientes;
+    select * from clientes;
+    select * from clientes where nome like "a%";
+    select idcli as ID, nome as cliente, email as Email, senha as Senha from clientes where nome like "a%";
+    
+    select senha from clientes where idcli=1;
+   -- buscar biblioteca rs2xml.jar Angels and Demons
+    
+    create table produtos (
 		codigo int primary key auto_increment,
         barcode varchar(50) unique,
-        nome varchar(100) not null,
+        produto varchar(100) not null,
         fabricante varchar(100) not null,
         -- ### Comando abaixo refere-se a data e hora automaticos ###
         datacad timestamp default current_timestamp,
         -- ### date é tipo de dados relacionados a data no formato YYYYMMDD ###
         dataval date not null,
-        quantidade int not null,
+        estoque int not null,
         estoquemin int not null,
         medida varchar(50) not null,
         valor decimal(10,2),
         loc varchar(100)
 );
 	
-describe estoque;
+	describe produtos;
     
-insert into  estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('caneta bic vermelho','BIC',20221005,100,10,'CX',28.75,'setor A p2');
-insert into estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('bota de couro','Dafit',20231005,150,5,'CX',192.70,'setor B  A5');
-insert into estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('chaveiro','suice',20251005,13,10,'CX',52.90,'setor C A2');
-insert into estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('carne','JBS',20191007,152,8,'PCT',179.90,'setor A p6');
-insert into estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('requeijão cheetos','Elma Chips',20201007,100,52,'PCT',9.90,'setor D p6');
-insert into estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('cerveja','skol',20201127,10,25,'LT',4.90,'setor c A6');
-insert into estoque(nome,fabricante,dataval,quantidade,estoquemin,medida,valor,loc) values ('vodka','russia',20211006,1,20,'GF',39.90,'setor A E9');
-select * from estoque;
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc) 
+values 
+('caneta bic vermelho','BIC',20221005,100,10,'CX',28.75,'setor A p2');
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc) 
+values ('bota de couro','Dafit',20231005,150,5,'CX',192.70,'setor B  A5');
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc) 
+values ('chaveiro','suice',20251005,13,10,'CX',52.90,'setor C A2');
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc)  
+values ('carne','JBS',20191007,152,8,'PCT',179.90,'setor A p6');
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc) 
+values ('requeijão cheetos','Elma Chips',20201007,100,52,'PCT',9.90,'setor D p6');
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc)  
+values ('cerveja','skol',20201127,10,25,'LT',4.90,'setor c A6');
+insert into produtos(produto,fabricante,dataval,estoque,estoquemin,medida,valor,loc) 
+values ('vodka','russia',20211006,1,20,'GF',39.90,'setor A E9');
+
+select * from produtos;
 
 -- Inventario de Estoque (Total)
-select sum(valor * quantidade) as total from estoque;
+select sum(valor * estoque) as total from produtos;
 
 -- Relatorio de reposição do estoque
-select * from estoque where quantidade < estoquemin;
+select * from produtos where estoque < estoquemin;
 
 -- Relatorio de reposição do estoque 2 
 -- date_format() -> fomatar a exibição da data
 -- %d = dia %m = mês %y = ano (2 digitos) %Y = ano (4 digitos)
-select codigo as código, nome, 
+select codigo as código, produto, 
 date_format(dataval,'%d/%m/%y') as data_validade,
-quantidade, estoquemin as estoque_minimo from estoque where quantidade < estoquemin;
+estoque, estoquemin as estoque_minimo from produtos where estoque < estoquemin;
 
 -- Relatório de produtos vencidos 1
-select codigo,nome,date_format(dataval,'%d/%m/%y') as data_validade from estoque;
+select codigo,produto,date_format(dataval,'%d/%m/%y') as data_validade from produtos;
 
 -- Relatório de validaden dos produtos 2
 -- curdate() = data atual
-select codigo as codigo, nome,date_format(dataval,'%d/%m/%y') as data_validade, datediff(dataval,curdate()) as dias_restantes from estoque;
+select codigo as codigo, produto,date_format(dataval,'%d/%m/%y') as data_validade, datediff(dataval,curdate()) as dias_restantes from produtos;
 
 -- ATIVIDADE 3 INDIVIDUAL
+-- 5 inserts ok || Selects personalizados ok || update simples | update completo | delete
 
+update produtos set estoque=12 where codigo=1;
+update produtos set nome='ailton',fabricante='chupa cabra', dataval=19910215,estoque=1,estoquemin=0,medida='GF',valor=1.00,loc='quinto' where codigo=3;
+delete from produtos where codigo=4; 
+    
+create table pedidos (
+	pedido int primary key auto_increment,
+    dataped timestamp default current_timestamp,
+    total decimal(10,2),
+    idcli int not null,
+    foreign key(idcli) references clientes(idcli)
+    );
+    
+insert into pedidos(idcli) values(1);
 
+-- Abertura do pedido
+select pedidos.pedido, 
+date_format(pedidos.dataped,'%d/%m/%Y - %H:%i') as data_pedido,
+clientes.nome as cliente, clientes.email as email from pedidos
+inner join clientes on pedidos.idcli = clientes.idcli;
+
+create table carrinho (
+	pedido int not null,
+    codigo int not null,
+    quantidade int not null,
+	foreign key(pedido) references pedidos(pedido),
+    foreign key(codigo) references produtos(codigo)
+    );
